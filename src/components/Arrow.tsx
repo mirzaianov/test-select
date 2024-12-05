f77b7be5a5c969bf52fd1104928df85f45ce715d
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-12-05 18:57:27 +0300

    feat: Add styles to Arrow

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,13 +1,9 @@
-type ArrowProps = {
-  className?: string;
-};
+import styled from './Arrow.module.css';
 
-export default function Arrow({ className }: ArrowProps) {
+export default function Arrow() {
   return (
     <svg
-      className={className}
-      width="10"
-      height="6"
+      className={styled.arrow}
       viewBox="0 0 10 6"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
